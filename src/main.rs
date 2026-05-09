e934d2a19a14660d6a357d3f57e177db28ac024d
Anurag Sharma <23anuragsharma@gmail.com>
2026-05-09 16:46:34 +0530

    basic rust server setup and simple route check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::{get, post}, Router};
+
+#[tokio::main]
+async fn main(){
+
+    let app = Router::new()
+    .route("/", get(root))
+    .route("/notes", post(create_note));
+ 
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Server running on http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
+
 }
+
+async fn root() -> &'static str{
+    "Hello, World!"
+}
+
+async fn create_note() -> &'static str{
+    "Note created!"
+}
+
+
